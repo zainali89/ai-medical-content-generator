d0709821cd0897116cac88cb6a84922d30d10b2f
Syed Basit Abbas <64358478+SyedBasitAbbas@users.noreply.github.com>
2025-03-13 14:44:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -540,4 +540,4 @@ async def generate_article(request: dict):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(fastapi_app, host="127.0.0.1", port=8000)
+    uvicorn.run(fastapi_app, host="0.0.0.0", port=8000)
