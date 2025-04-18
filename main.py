20f98fd223fe27966b34f2833ccab1bdae44fac7
Syed Basit Abbas <64358478+SyedBasitAbbas@users.noreply.github.com>
2025-04-18 10:50:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ class State(TypedDict):
     youtube_links: List[str]
     docs_data: List[str]
     youtube_data: List[str]
-    generated_content: strs
+    generated_content: str
     errors: Annotated[List[str], operator.add]
     performance_metrics: Annotated[Dict[str, float], lambda x, y: {**x, **y}]
     critical_error: Annotated[bool, lambda x, y: x or y]
@@ -367,6 +367,9 @@ def generate_content(state: State) -> dict:
     - Only include the links in the references.
     - If you're uncertain about any information, indicate this clearly rather than guessing.
     - For any statistical claims, medical recommendations, or specific treatments, cite the exact source from the reference data.
+    - Example:
+            Correct Youtube: (link) 
+            Not Correct: Youtube Transcript : (link)
     
     Keep the tone objective and evidence-based, current as of {current_date}, and note missing data if applicable. End with a reference list in this format:
     - [Number]. Title (Author(s), Date). Link: [URL]
